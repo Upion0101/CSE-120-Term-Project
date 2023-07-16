5e61814b4bce1af5c9f404bfe53271dd17ae1611
Dedra Dixon <109184770+DedraDixon@users.noreply.github.com>
2023-07-15 20:39:58 -0400

    Update main.py
    Added in scenery on the welcome page, added styling to all buttons, styled the see rules page

--- main.py
+++ main.py
@@ -4,20 +4,60 @@ import random
 score = 0
 player_dy = 0
 
+def opening_bg(canvas2):
+
+    # dark purple mountains left to right
+    canvas2.create_rectangle(0, 200, 500, 300, outline="darkslateblue", fill="darkslateblue", width=2)
+    canvas2.create_polygon(-150, 200, 0, 50, 50, 200, fill="darkslateblue", outline="darkslateblue")
+    canvas2.create_polygon(0, 200, 50, 80, 100, 200, fill="darkslateblue", outline="darkslateblue")
+    canvas2.create_polygon(100, 200, 175, 30, 250, 200, fill="darkslateblue", outline="darkslateblue")
+    canvas2.create_polygon(230, 200, 280, 60, 330, 200, fill="darkslateblue", outline="darkslateblue")
+    canvas2.create_polygon(245, 200, 295, 80, 355, 200, fill="darkslateblue", outline="darkslateblue")
+    canvas2.create_polygon(355, 200, 413, 20, 470, 200, fill="darkslateblue", outline="darkslateblue")
+    canvas2.create_polygon(450, 200, 500, 60, 550, 200, fill="darkslateblue", outline="darkslateblue")
+    # light purple mountains left to right
+    canvas2.create_rectangle(0, 205, 500, 500, outline="#8F8FBC", fill="#8F8FBC", width=2)
+    canvas2.create_polygon(-30, 205, 20, 100, 70, 205, fill="#8F8FBC", outline="#8F8FBC")
+    canvas2.create_polygon(50, 205, 100, 120, 150, 205, fill="#8F8FBC", outline="#8F8FBC")
+    canvas2.create_polygon(150, 205, 200, 110, 250, 205, fill="#8F8FBC", outline="#8F8FBC")
+    canvas2.create_polygon(180, 205, 230, 90, 280, 205, fill="#8F8FBC", outline="#8F8FBC")
+    canvas2.create_polygon(250, 205, 300, 125, 350, 205, fill="#8F8FBC", outline="#8F8FBC")
+    canvas2.create_polygon(325, 205, 375, 90, 425, 205, fill="#8F8FBC", outline="#8F8FBC")
+    canvas2.create_polygon(405, 205, 477, 90, 550, 205, fill="#8F8FBC", outline="#8F8FBC")
+    # sun and clouds
+    canvas2.create_oval(40, 30, 85, 75, outline="gold", fill="gold", width=2)
+    canvas2.create_oval(0, 50, 50, 55, fill="papaya whip", outline="papaya whip")
+    canvas2.create_oval(25, 55, 60, 60, fill="papaya whip", outline="papaya whip")
+    canvas2.create_oval(200, 68, 120, 75, fill="papaya whip", outline="papaya whip")
+    canvas2.create_oval(130, 72, 240, 78, fill="papaya whip", outline="papaya whip")
+    canvas2.create_oval(300, 72, 370, 78, fill="papaya whip", outline="papaya whip")
+    canvas2.create_oval(310, 70, 380, 72, fill="papaya whip", outline="papaya whip")
+    # first green dark green hill
+    canvas2.create_oval(-50, 300, 150, 578, fill="dark green", outline="dark green")
+    # overlap green hill on left
+    canvas2.create_oval(50, 320, 250, 478, fill="dark green", outline="dark green")
+    # right green hill
+    canvas2.create_oval(230, 578, 550, 320, fill="dark green", outline="dark green")
+    #bottom light green hill
+    canvas2.create_oval(-50, 415, 550, 600, fill="forest green", outline="forest green")
+    # river
+    coord = 235, 415, 265, 415, 310, 500, 190, 500
+    arc = canvas2.create_polygon(coord, fill="skyblue", outline="skyblue" )
+
 
 def runrules():
     # Removes the welcome page and displays the rules
     welcomecanvas.pack_forget()
-    rules_canvas = tk.Canvas(root, width=400, height=300)
-    rules_canvas.pack()
+    rules_canvas = tk.Canvas(root, bg="skyblue")
+    rules_canvas.pack(fill=tk.BOTH, expand=True)
     rules = 'Press <Space> to jump over obstacles'
-    rulesmessage = tk.Message(rules_canvas, text=rules)
-    rulesmessage.pack()
-    # Creates the go  button on the rules page
-    goback_button = tk.Button(rules_canvas, text='Go Back', cursor="left_side", width=25, command=lambda:
-    [rules_canvas.pack_forget(), welcomecanvas.pack()])
+    rulesmessage = tk.Message(rules_canvas, text=rules,  font=("Comic Sans MS", 16))
+    rulesmessage.pack(pady=(120, 60))
+    # Creates the go back button on the rules page
+    goback_button = tk.Button(rules_canvas, text='Go Back',  font=("Comic Sans MS", 14), width=15, height=2, command=lambda: [rules_canvas.pack_forget(),
+                                                                                       welcomecanvas.pack(fill=tk.BOTH, expand=True),
+                                                                                       opening_bg(welcomecanvas)])
     goback_button.pack(padx=10, pady=10)
-
 
 def rungame():
     def check_collision():
@@ -104,11 +144,14 @@ def rungame():
             canvas.create_text(345, 275, text="╯", fill="lemon chiffon")
             canvas.create_text(200, 300, text="Your score was " + str(score), font=("Helvetica", 24),
                                fill="lemon chiffon")
-            playagain_button = tk.Button(root, text='Play Again', cursor="exchange", width=25,
-                                         command=lambda: [canvas.pack_forget(), rungame(),
-                                                          playagain_button.pack_forget(), exit_button.pack_forget()])
+            playagain_button = tk.Button(root, text='Play Again', cursor="exchange", font=("Comic Sans MS", 14),
+                                         width=15, height=2, command=lambda: [canvas.pack_forget(),
+                                                                              rungame(),
+                                                                              playagain_button.pack_forget(),
+                                                                              exit_button.pack_forget()])
             playagain_button.pack()
-            exit_button = tk.Button(root, text='Exit', cursor="draft_large", width=25, command=root.destroy)
+            exit_button = tk.Button(root, text='Exit', cursor="draft_large", font=("Comic Sans MS", 14), width=15,
+                                    height=2, command=root.destroy)
             exit_button.pack()
             score = 0
             # reset score to zero after every turn
@@ -223,10 +266,10 @@ def rungame():
     obstacles = []
 
     # Create the start menu
-    start_button = tk.Button(root, text="Start", cursor="draft_large", command=start_game)
+    start_button = tk.Button(root, text="Start", cursor="draft_large", font=("Comic Sans MS", 14), width=15, height=2, command=start_game)
     start_button.pack()
 
-    # Run the GUI event loop
+
 
 
 if __name__ == '__main__':
@@ -235,22 +278,24 @@ if __name__ == '__main__':
     root.geometry("500x500")
     root.title("Mountain Jump Game")
 
+
     # Creates the canvas that the homepage(buttons and welcome) will appear on
-    welcomecanvas = tk.Canvas(root, width=400, height=400)
-    welcomecanvas.pack()
+    welcomecanvas = tk.Canvas(root, bg="skyblue")
+    welcomecanvas.pack(fill=tk.BOTH, expand=True)
+    opening_bg(welcomecanvas)
 
     # Creates the title on homepage
-    d = tk.Label(welcomecanvas, text="Welcome to Mountain Jump!", font=('Arial', 18))
-    d.pack(pady=40)
+    d = tk.Label(welcomecanvas, text="Welcome to Mountain Jump!", font=("Comic Sans MS", 18))
+    d.pack(pady=(160, 60))
 
     # Creates start game button
-    strt_button = tk.Button(welcomecanvas, text='Start Game', cursor="draft_large", width=25,
-                            command=lambda: [rungame(), welcomecanvas.pack_forget()])
-    strt_button.pack(pady=5)
+    strt_button = tk.Button(welcomecanvas, text='Start Game', cursor="draft_large", font=("Comic Sans MS", 14) , width=15, height=2, bg="skyblue", command=lambda: [rungame(),
+                                                                                         welcomecanvas.pack_forget()])
+    strt_button.pack()
 
     # Creates see rules button
-    rules_button = tk.Button(welcomecanvas, text='See Rules', cursor="draft_large", width=25, command=runrules)
-    rules_button.pack(pady=5)
+    rules_button = tk.Button(welcomecanvas, text='See Rules', cursor="draft_large", width=15, height=2, font=("Comic Sans MS", 14), command=runrules)
+    rules_button.pack(pady=15)
 
     # code below puts window in middle for user-- if this doesn't happen let me know
     width = 500  # Width
